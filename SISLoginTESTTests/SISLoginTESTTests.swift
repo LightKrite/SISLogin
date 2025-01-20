35851ba27c2371a74e2e8f2270ec81179f6e10c9
Egor Partenko <egorpartenko@icloud.com>
2025-01-20 13:17:00 +0100

    commit

---
+++ SISLoginTESTTests/SISLoginTESTTests.swift
@@ -0,0 +1,17 @@
+//
+//  SISLoginTESTTests.swift
+//  SISLoginTESTTests
+//
+//  Created by Егор Партенко on 20.1.25..
+//
+
+import Testing
+@testable import SISLoginTEST
+
+struct SISLoginTESTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
